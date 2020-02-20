251afd89218a13219e581c9b6c3af698d57b9140
Mahir <mahir.kadic@edu.fit.ba>
2020-02-20 20:36:40 +0100

    Setup of project with typescript

---
+++ mobile/App.tsx
@@ -0,0 +1,17 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
+// if (__DEV__) import('services/Reactotron');
+
+const App = () => {
+  return <View />;
+};
+
+export default App;
